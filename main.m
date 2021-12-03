cd6dac9d02490db02b9bbe0143c643d2fb798701
nobleRadical <88314680+nobleRadical@users.noreply.github.com>
2021-12-02 17:17:58 -0800

    preliminary variable setup

--- main.m
+++ main.m
@@ -6,7 +6,19 @@ This program (project)'s goal is to be a retirement age calculator.
 It should take some inputs, perhaps also a dataset for calculating averages, 
 and then deliver an age of retirement as a scalar 
 and the average range of retirement ages using a graph.
-
 %}
-% REMOVE THIS NEXT LINE AT PRODUCTION.
-defcon.setLevel(1);
+% REMOVE THIS NEXT LINE AT RELEASE.
+defcon.setLevel(5);
+
+age = 
+current_salary = 
+salary_expenses = 
+savings_year = 
+total_savings =
+asset_expense = 
+average_takes_year = 
+large_future_expenses =
+num_dependents =
+married =
+monthly_deductions =
+expected_pension =
