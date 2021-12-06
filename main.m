b7dff79e2add6df8b801765149441e9f834a353b
nobleRadical <88314680+nobleRadical@users.noreply.github.com>
2021-12-06 14:40:18 -0800

    basic varible setup

--- main.m
+++ main.m
@@ -10,15 +10,27 @@ and the average range of retirement ages using a graph.
 % REMOVE THIS NEXT LINE AT RELEASE.
 defcon.setLevel(5);
 
-age = 
-current_salary = 
-salary_expenses = 
-savings_year = 
-total_savings =
-asset_expense = 
-average_takes_year = 
-large_future_expenses =
-num_dependents =
-married =
-monthly_deductions =
-expected_pension =
+age = input("Enter your age:");
+current_salary = input("Enter your current salary:");
+salary_expenses = input("Enter your salary expenses (Things that come out of your salary consistently):");
+savings_year = input("Enter your savings earned per year:");
+total_savings = input("Enter your total current savings:");
+asset_expense = input("Enter your asset expenses (consistent expenses related to your car, house, etc:");
+average_taxes_year = input("What is your average tax payment per year? If you get a return, enter a negative number:");
+large_future_expenses = input("Do you plan any large future expenses? If so, enter their estimated total cost here:");
+num_dependents = input("How many dependents do you have/will have:");
+while true %loop to make the use actually enter an acceptable answer
+married = input("Are you married? y/n >", 's');
+if regexp(married,"[yY][eE]?[sS]?[1]?") % if yes
+    married = true;
+    break % leave the loop
+elseif regexp(married,"[nN][oO][0]?") % if no
+    married = false;
+    break % leave the loop
+else
+    disp("You entered neither a yes or no for 'married'.")
+end
+end
+monthly_deductions = input("What is the total value of any monthly deductions, per month:");
+expected_pension = input("What is your expected pension:");
+
